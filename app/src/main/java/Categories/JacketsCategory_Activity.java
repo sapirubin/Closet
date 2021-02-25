d4a7266a68f2756ecd77096e86d5f79abb226174
sapir rubin <sapirubin@gmail.com>
2021-02-25 21:16:07 +0200

    return button to the main for each category

--- app/src/main/java/Categories/JacketsCategory_Activity.java
+++ app/src/main/java/Categories/JacketsCategory_Activity.java
@@ -1,16 +1,46 @@
 package Categories;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mycloset.R;
 
 public class JacketsCategory_Activity extends AppCompatActivity {
+    private ImageView menu_IMG_ootd;
+    private ImageView menu_IMG_addPic;
+    private ImageView menu_IMG_homepage;
+    private Activity MainActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jackets);
+        menu_IMG_homepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMenu(MainActivity);
+
+
+            }
+        });
+
+
+    }
+
+    private void openMenu(Activity activity) {
+        Intent myIntent = new Intent(this, com.example.mycloset.MainActivity.class);
+        startActivity(myIntent);
+    }
+
+    private void findByView(){
+
+        menu_IMG_ootd= findViewById(R.id.menu_IMG_ootd);
+        menu_IMG_addPic= findViewById(R.id.menu_IMG_addPic);
+        menu_IMG_homepage= findViewById(R.id.menu_IMG_homepage);
 
 
     }
